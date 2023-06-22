fd8d887132e11db8b9cc081b4d22f4e4320d5014
osparking <jbpark03@gmail.com>
2023-06-22 14:08:48 +0900

    easy way to save record into rdb

---
+++ src/main/java/space/bumtiger/http/domain/HeavyRepository.java
@@ -0,0 +1,7 @@
+package space.bumtiger.http.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HeavyRepository extends 
+										CrudRepository<HeavyResource, Integer>{
+}
