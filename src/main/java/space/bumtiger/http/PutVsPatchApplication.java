f391648a7b96748e44c2c04ed66fe8a3171e77d3
osparking <jbpark03@gmail.com>
2023-06-22 13:01:12 +0900

    application class package set to http

--- src/main/java/space/bumtiger/http/PutVsPatchApplication.java
+++ src/main/java/space/bumtiger/http/PutVsPatchApplication.java
@@ -1,4 +1,4 @@
-package space.bumtiger.record;
+package space.bumtiger.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
