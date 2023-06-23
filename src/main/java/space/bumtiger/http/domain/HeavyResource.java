793d9165b570148bb3bd20ff64d56739c36e8b4a
osparking <jbpark03@gmail.com>
2023-06-23 09:52:05 +0900

    constructor for commandlinerunner

--- src/main/java/space/bumtiger/http/domain/HeavyResource.java
+++ src/main/java/space/bumtiger/http/domain/HeavyResource.java
@@ -1,9 +1,17 @@
 package space.bumtiger.http.domain;
 
+import org.springframework.data.annotation.Id;
+
 import lombok.Data;
 
 @Data
 public class HeavyResource {
+	public HeavyResource(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+
+	@Id
 	private Integer id;
 	private String name;
 	private String address;
