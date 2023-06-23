eebec7306a27b0b75f8f23e63616a13c55147450
osparking <jbpark03@gmail.com>
2023-06-23 12:46:50 +0900

    handler for address only update

--- src/main/java/space/bumtiger/http/controller/HeavyController.java
+++ src/main/java/space/bumtiger/http/controller/HeavyController.java
@@ -1,8 +1,11 @@
 package space.bumtiger.http.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import space.bumtiger.http.domain.HeavyRepository;
 import space.bumtiger.http.domain.HeavyResource;
+import space.bumtiger.http.domain.ResourceIDAddress;
 
 @RestController
 @RequestMapping("/heavyresource")
@@ -18,6 +22,18 @@ import space.bumtiger.http.domain.HeavyResource;
 public class HeavyController {
 	@Autowired
 	private HeavyRepository heavyRepository;
+
+	@PatchMapping
+	public ResponseEntity<?> patchResource(
+			@RequestBody ResourceIDAddress idAddress) {
+		Optional<HeavyResource> resource = heavyRepository
+				.findById(idAddress.getId());
+		if (resource.isPresent()) {
+			resource.get().setAddress(idAddress.getAddress());
+			heavyRepository.save(resource.get());
+		} 
+		return ResponseEntity.of(resource);
+	}
 
 	@PutMapping
 	public ResponseEntity<?> saveResource(
@@ -30,5 +46,5 @@ public class HeavyController {
 	public Iterable<HeavyResource> getAllHeavyResources() {
 		return heavyRepository.findAll();
 	}
-	
+
 }
