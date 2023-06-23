2d056234cb2da09015a69302510e37aa7bf503e3
osparking <jbpark03@gmail.com>
2023-06-23 09:16:47 +0900

    controller for heavy resource

---
+++ src/main/java/space/bumtiger/http/controller/HeavyController.java
@@ -0,0 +1,11 @@
+package space.bumtiger.http.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController("/heavyresource")
+@RequiredArgsConstructor
+public class HeavyController {
+
+}
