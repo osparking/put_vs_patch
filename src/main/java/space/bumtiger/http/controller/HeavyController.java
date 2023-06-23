7a99de6d87491e05b6b6f9c3ee88779896b5a98d
osparking <jbpark03@gmail.com>
2023-06-23 11:10:09 +0900

    update heavy_resource tuple as a whole

--- src/main/java/space/bumtiger/http/controller/HeavyController.java
+++ src/main/java/space/bumtiger/http/controller/HeavyController.java
@@ -1,11 +1,26 @@
 package space.bumtiger.http.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import space.bumtiger.http.domain.HeavyRepository;
+import space.bumtiger.http.domain.HeavyResource;
 
 @RestController("/heavyresource")
 @RequiredArgsConstructor
 public class HeavyController {
+	@Autowired
+	private HeavyRepository heavyRepository;
+
+	@PutMapping
+	public ResponseEntity<?> saveResource(
+			@RequestBody HeavyResource heavyResource) {
+		heavyRepository.save(heavyResource);
+		return ResponseEntity.ok("자원 저장됨");
+	}
 
 }
