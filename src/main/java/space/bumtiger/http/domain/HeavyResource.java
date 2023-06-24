6421d10e576ee1a961c130fc3de1157b80d100fe
osparking <jbpark03@gmail.com>
2023-06-24 17:35:00 +0900

    constructors for JUnit test case

--- src/main/java/space/bumtiger/http/domain/HeavyResource.java
+++ src/main/java/space/bumtiger/http/domain/HeavyResource.java
@@ -2,9 +2,13 @@ package space.bumtiger.http.domain;
 
 import org.springframework.data.annotation.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HeavyResource {
 	public HeavyResource(String name, String address) {
 		this.name = name;
